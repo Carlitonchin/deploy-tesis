410d868124feb603ab226ece04380465c5ae0e7f
Carlitonchin <carlozalejandro98@gmail.com>
2022-09-06 19:12:09 -0300

    update postgres db env

--- data_source.go
+++ data_source.go
@@ -17,11 +17,11 @@ type dataSource struct {
 }
 
 func init_db() (*dataSource, error) {
-	db_host := os.Getenv("DB_HOST")
-	db_port := os.Getenv("DB_PORT")
-	db_user := os.Getenv("DB_USER")
-	db_pass := os.Getenv("DB_PASS")
-	db_name := os.Getenv("DB_NAME")
+	db_host := os.Getenv("PGHOST")
+	db_port := os.Getenv("PGPORT")
+	db_user := os.Getenv("PGUSER")
+	db_pass := os.Getenv("PGPASSWORD")
+	db_name := os.Getenv("PGDATABASE")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", db_host, db_user, db_pass, db_name, db_port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
